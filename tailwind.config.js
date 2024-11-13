53015466980be24914fd88413687455b25fc78df
Adnan Rashid <arasheed@teresol.com>
2024-11-13 12:41:48 +0500

    simple page with full responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
         'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
   },
